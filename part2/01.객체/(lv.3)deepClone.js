be4631cb0de6e7910d06db74f4c5ddae0c35c42f
lyra <mk.lyra.j@gmail.com>
2025-03-12 09:38:40 +0900

    ✨ Update : 깊은복사는 알다가도 모르겟군요

--- part2/01.객체/(lv.3)deepClone.js
+++ part2/01.객체/(lv.3)deepClone.js
@@ -8,7 +8,23 @@
  * @returns {*} 깊은 복사된 결과
  */
 
-function deepClone(obj) {}
+function deepClone(obj) {
+  // 1. 기본 타입은 깊은복사가 필요없으므로 그대로 반환(string, number, boolean, null, undefiend)
+  if (typeof obj !== "object" || obj === null) {
+    return obj;
+  }
+
+  // 2. 배열과 객체에 때라 새로운 복사본을 초기화
+  const clone = Array.isArray(obj) ? [] : {};
+
+  // 3. 객체의 각 속성을 순회하면서 재귀적으로 깊은복사 실행
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      clone[key] = deepClone(obj[key]);
+    }
+  }
+  return clone;
+}
 
 // export 를 수정하지 마세요.
 export { deepClone };
